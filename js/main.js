f40c08b53f1228b7519e07faab89f7adc43c3721
fatemehghorbani <fatemeh.ghorbani0078@gmail.com>
2022-08-16 09:12:20 -0700

    Upload sing up on github

--- js/main.js
+++ js/main.js
@@ -1,51 +1,75 @@
-
 function validate() {
-    var email = document.getElementById("form3Example3").value;
-    var password = document.getElementById("form3Example4").value;
-  
-    if (contn(password) == false) {
-      alert("رمز به درستی تعریف نشده است");
-      return false;
-    }
-  
-    if (contlowerCaseLetters(password) == false) {
-      alert(" رمز دارای حروف کوچک باشد ");
-      return false;
-    }
-  
-    if (contupperCaseLetters(password) == false) {
-      alert("رمز دارای حروف بزرگ باشد");
-      return false;
-    }
+  var email = document.getElementById("form3Example3").value;
+  var password = document.getElementById("form3Example4").value;
+  var Enterpassword = document.getElementById("form3Example5").value;
+  var firstName = document.getElementById("form3Example1").value;
+  var lastName = document.getElementById("form3Example2").value;
+
+  if (
+    contlowerCaseLetters(firstName) == false &&
+    contnupperCaseLetters(firstName) == false
+  ) {
+    alert(" نام کاربر را به درستی وراد کنید ");
+    return false;
   }
-  function contn(cont) {
-    return /[0-9]/.test(cont);
+
+  if (
+    contlowerCaseLetters(lastName) == false &&
+    contnupperCaseLetters(lastName) == false
+  ) {
+    alert("نام خانوادگی را به درستی وارد کنید ");
+    return false;
   }
-  
-  function uservalidate(email) {
-    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    email.value = email.value.replace(regex, "");
-  }q
 
-  
-  
-  function passvalidate(pass) {
-    var regex = /[^0-9A-Za-z#$&@^_*]/;
-    pass.value = pass.value.replace(regex, "");
+  // if ((uservalidate(email) == false)) {
+  //   alert("ایمیل به درستی تعریف نشده است");
+  //   return false;
+  // }
+
+  if (contn(password) == false) {
+    alert("رمز به درستی تعریف نشده است");
+    return false;
   }
-  
-  function contlowerCaseLetters(contlowerCaseLetters) {
-    return /[a-z]/.test(contlowerCaseLetters);
+
+  if (contlowerCaseLetters(password) == false) {
+    alert(" رمز دارای حروف کوچک باشد ");
+    return false;
   }
-  
-  function contnupperCaseLetters(contupperCaseLetters) {
-    return /[A-Z]/.test(contupperCaseLetters);
+
+  if (contnupperCaseLetters(password) == false) {
+    alert("رمز دارای حروف بزرگ باشد");
+    return false;
   }
-  
 
+  // if(!(password==Enterpassword)){
+  //   alert(تکرار رمز اشتباه  است )
+  //   return false;
+  // }
+}
 
+function contn(cont) {
+  return /[0-9]/.test(cont);
+}
 
+function contlowerCaseLetters(contlowerCaseLetters) {
+  return /[a-z]/.test(contlowerCaseLetters);
+}
 
+function contnupperCaseLetters(contupperCaseLetters) {
+  return /[A-Z]/.test(contupperCaseLetters);
+}
 
+// function uservalidate(email) {
+//   var regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+//   email.value = email.value.replace(regex, "");
+// }
 
+function passvalidate(pass) {
+  var regex = /[^0-9A-Za-z#$&@^_*]/;
+  pass.value = pass.value.replace(regex, "");
+}
 
+function confpassword(confpassword) {
+  var password = $("#form3Example4").val();
+  var Enterpassword = $("#form3Example5").val();
+}
